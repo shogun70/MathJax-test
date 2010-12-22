0cb6501fb147b9b16f3b08094eb21fd1c36bf2d8
Sean Hogan <shogun70@gmail.com>
2010-12-23 09:49:57 +1100

    Update / fix presentation

--- tools/diagnostix.js
+++ tools/diagnostix.js
@@ -1,8 +1,29 @@
+/*************************************************************
+ *
+ *  diagnostix.js
+ *  
+ *  ---------------------------------------------------------------------
+ *  
+ *  Copyright (c) 2010 Design Science, Inc.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 (function() {
-var root = MathJax.Hub.config.root;
+var root = MathJaxToolsDir
 var height = 300;
 var img = document.createElement("img");
-img.src = root + "/tools/logo.gif";
+img.src = root + "/logo.gif";
 img.width = "100";
 img.height = "20";
 img.style.cssText = "position: fixed; bottom: 0px; right: 10px; background-color: #fff; padding: 0.5em; border: 2px solid #ccc; " +
@@ -16,7 +37,7 @@ document.body.appendChild(div);
 var iframe = document.createElement("iframe");
 iframe.style.cssText = "position: fixed; bottom: 0px; left: 0px; width: 100%; height: 0px; overflow: " +
 	"hidden; background-color: #fff; border: none; border-top: 2px solid #ccc;";
-iframe.src = root + "/tools/diagnostix.html";
+iframe.src = root + "/diagnostix.html";
 document.body.appendChild(iframe);
 
 img.onclick = function() {
